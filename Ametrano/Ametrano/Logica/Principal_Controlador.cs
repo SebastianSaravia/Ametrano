3b1fad6578c141d631b26063de7e8109f6b5201f
Junsei-02 <Junsei@PC04-A1>
2018-08-30 20:34:07 -0300

    se agrego funcionalidad para verificar placeholders

--- Ametrano/Ametrano/Logica/Principal_Controlador.cs
+++ Ametrano/Ametrano/Logica/Principal_Controlador.cs
@@ -9,11 +9,57 @@ namespace Ametrano.Logica
 {
     class Principal_Controlador
     {
+        public IDictionary<string, string> mapTextBox = new Dictionary<string, string>();
         public void abrirConfiguracion()
         {
             Configuracion ventanaConfiguracion = new Configuracion();
             ventanaConfiguracion.Show();
         }
+
+        private void placeholderEventEnter(object sender, EventArgs e)
+        {
+
+            placeholder((TextBox)sender, "enter");
+        }
+        private void placeholderEventLeave(object sender, EventArgs e)
+        {
+            placeholder((TextBox)sender, "leave");
+        }
+
+        public TextBox placeholder(TextBox componente, string evento)
+        {
+            string textoCuandoEstaVacio = "";
+            controlador.mapTextBox.TryGetValue(componente.GetHashCode().ToString(), out textoCuandoEstaVacio);
+
+            if (evento.Equals("enter"))
+            {
+                if (componente.Text.Equals(textoCuandoEstaVacio))
+                {
+                    componente.Text = "";
+                    componente.ForeColor = Color.Black;
+                }
+
+            }
+            else
+            {
+
+                if (componente.Text.Equals(""))
+                {
+
+                    componente.Text = textoCuandoEstaVacio;
+                    componente.ForeColor = Color.Gray;
+                }
+
+
+            }
+
+            return null;
+        }
+
+
+
+
+
 
 
 
