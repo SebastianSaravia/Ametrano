678039074f50ac79671a7f4c1e4275803155f785
Junsei-02 <Junsei@PC04-A1>
2018-09-03 17:16:28 -0300

    se agrego consultarPersona

--- Ametrano/Ametrano/Logica/Principal_Controlador.cs
+++ Ametrano/Ametrano/Logica/Principal_Controlador.cs
@@ -1,7 +1,9 @@
 ﻿using Ametrano.Persistencia;
 using Ametrano.Presentacion;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ namespace Ametrano.Logica
 {
     class Principal_Controlador
     {
+        TestingClass testing = new TestingClass();//--------------------------------------
         public IDictionary<string, string> mapTextBox = new Dictionary<string, string>();
         ConexionBD objetoConexion = new ConexionBD();
 
@@ -57,11 +60,78 @@ namespace Ametrano.Logica
             }
 
 
-            
-
 
             return retorno;
         }
+        public dynamic[] consultarPersona(int tipoPersona,int tipoConsulta, string datoDeBusqueda) 
+        {
+
+
+            bool retorno = false; //Variable de retorno
+            /*TipoPersona explicacion:
+             * Variable que determina si la busqueda se enfoca en un docente o alumno
+             * tipoPersona==0 -> docente
+             * tipoPersona==1 -> alumno
+             */
+
+            /*TipoConsulta explicacion:
+             * Variable que determina si la busqueda se realiza por cedula o nombre y apellido
+             * tipoConsulta==0 -> cedula
+             * tipoConsulta==1 -> nombre y apellido
+             */
+            string consulta = "";
+            if(tipoPersona == 0)
+            {//Si es un docente
+                if(tipoConsulta == 0)
+                {//Si es por cedula
+                    consulta = "SELECT * FROM DOCENTE WHERE cedula_docente = '" + datoDeBusqueda + "';";
+                }else
+                {//Si es un nombre y apellido
+                    consulta = "SELECT * FROM DOCENTE WHERE CONCAT(nombre1,' ',apellido1) LIKE = '%" + datoDeBusqueda + "%';";
+                }
+            }else
+            {//Si es un alumno
+                if (tipoConsulta == 0)
+                {//Si es por cedula
+                    consulta = "SELECT * FROM ALUMNO WHERE cedula_alumno = '" + datoDeBusqueda + "';";
+                }
+                else
+                {//Si es un nombre y apellido
+                    consulta = "SELECT * FROM ALUMNO WHERE CONCAT(nombre1,' ',apellido1) LIKE = '%" + datoDeBusqueda + "%';";
+                }
+            }
+            
+            MySqlDataAdapter datos = objetoConexion.consultarDatos(consulta);//Ejecuto la consulta
+
+            DataTable tablaDeDatos = new DataTable();
+            datos.Fill(tablaDeDatos);
+            IDictionary<string, string> mapaDeDatos = new Dictionary<string,string>();
+
+            foreach(DataRow row in tablaDeDatos.Rows)
+            {
+                foreach(DataColumn column in tablaDeDatos.Columns)
+                {
+                    mapaDeDatos.Add(column.ColumnName.ToString(), row[column].ToString());
+                    testing.MostrarMessageBox(column.ColumnName.ToString() + " --- " + row[column].ToString());
+                }
+            }
+            retorno = true;
+
+            dynamic[] datosParaRetornar = new dynamic[2];
+            /*Explicacion datosParaRetornar
+             * datosParaRetornar[0] == -> variable bool que determina si la consulta se realizo con exito
+             * datosParaRetornar[1] == -> IDictionary que almacena los datos
+             */
+
+            datosParaRetornar[0] = retorno;
+            datosParaRetornar[1] = mapaDeDatos;
+
+
+            return datosParaRetornar;
+        }
+
+
+        
 
        
 
