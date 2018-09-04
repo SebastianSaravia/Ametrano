3b6b5c902a6f1e21fc54ec994cd65efcbc937e5c
Junsei-01 <Junsei@PC07-A1>
2018-09-04 15:58:42 -0300

    se agrego la clase consultas SQL

---
+++ Ametrano/Ametrano/Persistencia/ConsultasSQL.cs
@@ -0,0 +1,15 @@
+﻿namespace Ametrano.Persistencia
+{
+    class ConsultasSQL
+    {
+        public string agregarDocente() {
+            string query = "INSERT INTO docente VALUES(@cedula,@nombre1,@nombre2,@apellido1,@apellido2,@direccion,@telefono,@email)";
+            return query;
+        }
+
+
+
+
+
+    }
+}
